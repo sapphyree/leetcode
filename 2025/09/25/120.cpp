a6da84ff8faf9468ae8ec64f0d60ea86e82f9c4d
sapphyree <sonexix@gmail.com>
2025-09-25 15:28:49 +0100

    Solve 120 via dynamic programming approach.

--- 2025/09/25/120.cpp
+++ 2025/09/25/120.cpp
@@ -1,11 +1,13 @@
 #include <vector>
 #include <iostream>
+#include <algorithm>
+
 
 using namespace std;
 
 class Solution {
 public:
-	int minimumTotal(vector<vector<int>> &triangle){
+	int minimumTotalOld(vector<vector<int>> &triangle){
        		int sumOfMinimumPath = 0;
 		int currentLevelIndex = 0;
 
@@ -41,6 +43,25 @@ public:
 		
 		return sumOfMinimumPath;
 	}
+	// walk backwards up the path
+	int minimumTotal(vector<vector<int>> &triangle){
+       		int sumOfMinimumPath = 0;
+		
+		// loop through both i,j, starting i at bottom of list and going up
+		for (int i= triangle.size() - 2; i >= 0; i--) {
+			for (int j = 0; j < triangle[i].size(); j++) {
+				// set current element to the minimum of the two boxes below (i.e i+1, j, i+1, j+1)
+				triangle[i][j] += min(triangle[i+1][j], triangle[i+1][j+1]);
+				cout << triangle[i][j] << endl;
+			}
+
+			
+		}
+
+		sumOfMinimumPath = triangle[0][0];
+
+		return sumOfMinimumPath;
+	}
 };
 
 int main() {
@@ -53,7 +74,9 @@ int main() {
 	vector<vector<int>> testCase3 = {{0}};
 	vector<vector<int>> testCase4 = {{0}, {0,0}};
 	vector<vector<int>> testCase5 = {{2}, {-3,4}, {100, 99, 7}, {4,1,-8,3}};
-	vector<vector<int>> testCase6 = {{3},{3,3},{3,3,3},{3,3,3,3},{3,3,3,3,3},{3,3,3,3,3,0,3}};
+	vector<vector<int>> testCase6 = {{3},{3,3},{3,3,3},{3,3,3,3},{3,3,3,3,3},{3,3,3,3,0,3}};
+
+	//sol.minimumTotal(testCase5);
 
 	cout << sol.minimumTotal(testCase1) << endl;
 	cout << sol.minimumTotal(testCase2) << endl;
